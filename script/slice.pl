f1135205d8e87281a98cd3af238adbbce94c5c7b
m <mmacnair.org>
2024-08-25 14:53:25 +0100

    rework to new db format, critical path complete

--- script/slice.pl
+++ script/slice.pl
@@ -3,9 +3,9 @@ use strict;
 use warnings;
 
 # ABSTRACT: run SlicerController::import_work_list()
-our $VERSION = 'v1.0.5';
+our $VERSION = 'v1.0.6';
 
-##~ DIGEST : a21869977306dc34a5283bf0f2432c67
+##~ DIGEST : 9e3c5625f6405ed6788aeaa4a6d5f3b5
 
 BEGIN {
 	push( @INC, "./lib/" );
@@ -21,32 +21,7 @@ sub main {
 	die "Block id not provided" unless $block_id;
 	$self->_do_db( {} );
 
-	my $rows = $self->query(
-		"select 
-			f.file_path,
-			f.x_dimension ,
-			f.y_dimension, 
-			p.x_position ,
-			p.y_position,
-			p.rotate,
-			p.rowid
-		from files f 
-		join projects p 
-			on f.rowid = p.file_id 
-		where 
-			 project_block  =?
-		order by 
-
-			p.y_position	ASC,
-			p.x_position	ASC
-			", $block_id
-	);
-	while ( my $row = $rows->fetchrow_hashref() ) {
-
-		$self->adjust_sleep_for_file( $row->{file_path} );
-	}
-
-	$self->slice_and_save( lc( $block_id ) );
+	$self->slice_and_save_plate( lc( $block_id ) );
 	$self->play_end_sound();
 }
 1;
